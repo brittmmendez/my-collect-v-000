d844c04484b0878f48b9ebfeae7b0853b4e4fe9a
Brittany Morris <brittmmendez@gmail.com>
2017-12-01 16:27:32 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(array)
   i=0
   new_collect=[]
   while i<array.length
-    new_collect<<yield my_collect(array[i])
+    new_collect<<yield(array[i])
     i+=1
   end
     new_collect
